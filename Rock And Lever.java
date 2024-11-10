6cfbbbf540355fa15b6c57c7fe7e7bb15cd863dc
Mohith Kumar <155897668+b0tProgrammer@users.noreply.github.com>
2024-11-10 12:07:34 +0530

    Add files via upload

---
+++ Rock And Lever.java
@@ -0,0 +1,238 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static final long MAX = (long) 1e9 + 7; // max size for MOD
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+
+//            Comparator<Pair> com = (i, j) -> (int) ((i.val - j.val));
+
+            int testcases = 1;
+
+            testcases = in.nextInt();
+
+            while (testcases-- > 0) {
+                /*
+                    Rock And lever!
+                    In this we have to find the total number of pairs such that (i<j) &&
+                    ai&aj >= ai^aj;
+
+                 */
+                int siz = in.nextInt();
+                int[] nums = new int[siz];
+                boolean[] vis = new boolean[siz]; // to mark the indices that are visited!
+                for(int i = 0; i < siz; i++) nums[i] = in.nextInt();
+                long pairs = 0;
+                for(int i = 31; i >= 0; i--) { // counting the number of set bits at the ith position!
+                    long c = 0;
+                    for(int j = 0; j < siz; j++) {
+                        if ((nums[j] & (1 << i)) != 0 && !vis[j]) {
+                            c++;
+                            vis[j] = true;
+                        }
+                    }
+                    pairs += (c*(c-1))>>1;
+                }
+                out.println(pairs);
+            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private static int makePalindrome(String s, char c) {
+        int count = 0;
+        int l = 0;
+        int r = s.length()-1;
+        while(l < r) {
+            if(s.charAt(l) != s.charAt(r)) {
+                if(s.charAt(l) == c) {
+                    l++; count++;
+                }
+                else if (s.charAt(r) == c) {
+                    r--; count++;
+                }
+                else return -1;
+            }
+            else {
+                l++; r--;
+            }
+        }
+        return count;
+    }
+
+//<--------------------------------------Fast reader----------------------------------------->
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+//<---------------------------------------Fast writer--------------------------------------------->
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println() throws IOException {
+            bw.append("\n");
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+// <-------------------------------------------------Some helper methods---------------------------------------->
+
+    public static long fastPow(long b, long e) {
+        long res = 1;
+        while (e > 0) {
+            if (odd(e)) res = (res * b) % MAX;
+            b = (b * b) % MAX;
+            e = e >> 1;
+        }
+        return res;
+    }
+
+    public static int charToInt(char c) {
+        return (c - '0');
+    }
+
+    public static char intToChar(int n) {
+        return (char) (n + 48);
+    }
+
+    public static int[] sieve(int upto) {
+        int[] primes = new int[upto + 1];
+        Arrays.fill(primes, 1);
+        for (int i = 2; i * i <= upto; i++) {
+            if (primes[i] == 1)
+                for (int j = i * i; j <= upto; j += i) {
+                    primes[j] = 0;
+                }
+        }
+        return primes;
+    }
+
+    private static long gcd(long a, long b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static int gcd(int a, int b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static long sumN(long siz) {
+        return (siz * (siz + 1)) >> 1;
+    }
+
+    private static boolean odd(long siz) {
+        return (siz & 1) == 1;
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static long max(long a, long b) {
+        return Math.max(a, b);
+    }
+
+    private static boolean powOf2(long siz) {
+        return (siz & (siz - 1)) == 0;
+    }
+
+    private static List<Integer> primeFactors(int siz) {
+
+        List<Integer> sizs = new ArrayList<>();
+        int[] p = new int[siz + 1];
+        p = sieve(siz);
+
+        for (int i = 2; i <= siz; i++) {
+            while (siz % i == 0 && p[i] == 1) {
+                sizs.add(i);
+                siz /= i;
+            }
+        }
+        return sizs;
+    }
+}
+
+class Pair2 {
+    long v1;
+    long v2;
+
+    public Pair2(long v1, long v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+}
+
+class Pair {
+    int val;
+    int idx;
+
+    public Pair(int val, int idx) {
+        this.val = val;
+        this.idx = idx;
+    }
+}
